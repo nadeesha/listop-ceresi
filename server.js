827799e6984065b0d8901b87da928fa4c52e8c3e
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-09-26 00:16:45 +0530

    api spec done

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 
 // Constants
@@ -5,9 +7,14 @@ var PORT = 8080;
 
 // App
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+app.get('/api/v1/:app/:action', function (req, res) {
+  return res.send(200, {
+  	app: req.params.app,
+  	action: req.params.action
+  });
 });
 
 app.listen(PORT);
+
 console.log('Running on http://localhost:' + PORT);
